708f25b4fe8a9f6bb844ccc2e141e16b4279a5b0
ItzArima <skaypaprilia@gmail.com>
2021-10-29 19:55:33 +0200

    esercizio rifatto con v-if

--- main.js
+++ main.js
@@ -1,37 +1,7 @@
-let counter =0;
-
-const slides = [
-    {
-        title:"Svezia",
-        image:"./img/01.jpg",
-        text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.",
-    },
-    {
-        title:"Svizzera",
-        image:"./img/02.jpg",
-        text:"Lorem ipsum",
-    },
-    {
-        title:"Gran Bretagna",
-        image:"./img/03.jpg",
-        text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-        title:"Germania",
-        image:"./img/04.jpg",
-        text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
-    },
-    {
-        title:"Paradise",
-        image:"./img/05.jpg",
-        text:"Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,",
-    },
-]
-
 const app = new Vue({
     el : "#root",
     data: {
-        counter : 0,
+        counter:0,
         slides: [
             {
                 title:"Svezia",
@@ -63,37 +33,24 @@ const app = new Vue({
     methods: {
         up(){
             console.log("up");
-            if(counter==0){
-                counter=4;
+            if(app.counter==0){
+                app.counter=4;
             }
             else{
-                counter--;
+                app.counter--;
             }    
-            console.log(counter);
-            document.getElementById("selected-img").innerHTML = `
-                <img class="selected-img" src="${slides[counter].image}" alt="" srcset="">
-                <div class="image-text">
-                    <h2 id="selected-title">${slides[counter].title}</h2>
-                    <p id="selecterd text">${slides[counter].text}</p>
-                </div> 
-            `
+            console.log(app.counter);
         },
         down(){
             console.log("down");
-            if(counter==4){
-                counter=0;
+            if(app.counter==4){
+                app.counter=0;
             }
             else{
-                counter++;
+                app.counter++;
             }    
-            console.log(counter);
-            document.getElementById("selected-img").innerHTML = `
-                <img class="selected-img" src="${slides[counter].image}" alt="" srcset="">
-                <div class="image-text">
-                    <h2 id="selected-title">${slides[counter].title}</h2>
-                    <p id="selecterd text">${slides[counter].text}</p>
-                </div> 
-            `
+            console.log(app.counter);
+
         }
     }
        
